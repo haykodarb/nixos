d52f33aac46dfef6d9b834587ec09c14b89433e9
Hayk Darbinyan <work@hayk.ar>
2024-08-30 14:42:04 -0300

    78 current  2024-08-30 14:37:24  24.05.4326.36bae4507766  6.10.6                          *

--- rebuild.zsh
+++ rebuild.zsh
@@ -1,11 +1,12 @@
 #!/usr/bin/env zsh
 set -e
 pushd ~/nixos
-nvim ./user/home.nix
+nvim ~/nixos
 alejandra . &>/dev/null
 git diff -U0 *.nix 
 echo "NixOS Rebuilding"
 sudo nixos-rebuild switch &> nixos-switch.log || (cat nixos-switch.log | grep --color error && false)
 gen=$(nixos-rebuild list-generations | grep current)
 git commit -am "$gen"
+git push
 popd
