374d0ad7b9d32ffeeabb2068f84b2b397bed1358
Hayk Darbinyan <work@hayk.ar>
2024-08-31 22:05:51 -0300

    101 current  2024-08-31 22:05:46  24.05.4326.36bae4507766  6.10.6                          *

--- configuration.nix
+++ configuration.nix
@@ -109,6 +109,12 @@
   #};
 
   programs.firefox.enable = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
 
   services.displayManager.sddm.catppuccin.enable = false;
   boot.loader.grub.catppuccin.enable = true;
