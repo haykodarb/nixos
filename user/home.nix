2e18274814188605d90a8a2bccc51228dd6842ca
Hayk Darbinyan <work@hayk.ar>
2024-09-06 22:54:33 -0300

    156 current  2024-09-06 22:54:20  24.05.4326.36bae4507766  6.10.6                          *

--- user/home.nix
+++ user/home.nix
@@ -267,6 +267,28 @@ in {
           extensions = {
             dap-ui = {
               enable = true;
+              # Set icons to characters that are more likely to work in every terminal.
+          # Feel free to remove or use ones that you like more! :)
+          # Don't feel like these are good choices.
+          icons = {
+            expanded = "▾";
+            collapsed = "▸";
+            current_frame = "*";
+          };
+
+          controls = {
+            icons = {
+              pause = "⏸";
+              play = "▶";
+              step_into = "⏎";
+              step_over = "⏭";
+              step_out = "⏮";
+              step_back = "b";
+              run_last = "▶▶";
+              terminate = "⏹";
+              disconnect = "⏏";
+            };
+          };
              # layouts = [
               #  {
                #   elements = [
