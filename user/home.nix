04f744ca64d6014c49520d44177b1888111c9d72
Hayk Darbinyan <work@hayk.ar>
2024-09-06 23:00:28 -0300

    157 current  2024-09-06 23:00:14  24.05.4326.36bae4507766  6.10.6                          *

--- user/home.nix
+++ user/home.nix
@@ -315,80 +315,88 @@ in {
             #  ];
             };
           };
-          # https://nix-community.github.io/nixvim/keymaps/index.html
-          keymaps = [
-            {
-              mode = "n";
-              key = "<F5>";
-              action.__raw = ''
-              function()
-              require('dap').continue()
-              end
-              '';
-              options = {
-                desc = "Debug: Start/Continue";
-              };
-            }
-            {
-              mode = "n";
-              key = "<F1>";
-              action.__raw = ''
-              function()
-              require('dap').step_into()
-              end
-              '';
-              options = {
-                desc = "Debug: Step Into";
-              };
-            }
-            {
-              mode = "n";
-              key = "<F2>";
-              action.__raw = ''
-              function()
-              require('dap').step_over()
-              end
-              '';
-              options = {
-                desc = "Debug: Step Over";
-              };
-            }
-            {
-              mode = "n";
-              key = "<F3>";
-              action.__raw = ''
-              function()
-              require('dap').step_out()
-              end
-              '';
-              options = {
-                desc = "Debug: Step Out";
-              };
-            }
-            {
-              mode = "n";
-              key = "<leader>b";
-              action.__raw = ''
-              function()
-              require('dap').toggle_breakpoint()
-              end
-              '';
-              options = {
-                desc = "Debug: Toggle Breakpoint";
-              };
-            }
-            {
-              mode = "n";
-              key = "<leader>B";
-              action.__raw = ''
-              function()
-              require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-              end
-              '';
-              options = {
-                desc = "Debug: Set Breakpoint";
-              };
-            }
+      };
+    };
+
+    extraPlugins = with pkgs.vimPlugins; [
+      gruvbox
+      vim-nix
+      lspkind-nvim
+    ];
+
+    keymaps = [
+      {
+        mode = "n";
+        key = "<F5>";
+        action.__raw = ''
+        function()
+        require('dap').continue()
+        end
+        '';
+        options = {
+          desc = "Debug: Start/Continue";
+        };
+      }
+      {
+        mode = "n";
+        key = "<F1>";
+        action.__raw = ''
+        function()
+        require('dap').step_into()
+        end
+        '';
+        options = {
+          desc = "Debug: Step Into";
+        };
+      }
+      {
+        mode = "n";
+        key = "<F2>";
+        action.__raw = ''
+        function()
+        require('dap').step_over()
+        end
+        '';
+        options = {
+          desc = "Debug: Step Over";
+        };
+      }
+      {
+        mode = "n";
+        key = "<F3>";
+        action.__raw = ''
+        function()
+        require('dap').step_out()
+        end
+        '';
+        options = {
+          desc = "Debug: Step Out";
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>b";
+        action.__raw = ''
+        function()
+        require('dap').toggle_breakpoint()
+        end
+        '';
+        options = {
+          desc = "Debug: Toggle Breakpoint";
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>B";
+        action.__raw = ''
+        function()
+        require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+        end
+        '';
+        options = {
+          desc = "Debug: Set Breakpoint";
+        };
+      }
       # Toggle to see last session result. Without this, you can't see session output
       # in case of unhandled exception.
       {
@@ -403,17 +411,8 @@ in {
           desc = "Debug: See last session result.";
         };
       }
-    ];
-      };
-    };
 
-    extraPlugins = with pkgs.vimPlugins; [
-      gruvbox
-      vim-nix
-      lspkind-nvim
     ];
-
-    keymaps = [];
     
     colorschemes.rose-pine.enable = true;
     colorschemes.rose-pine.settings = {
