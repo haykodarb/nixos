449732a2d7536a7695874e8a0a94ddcc678cee2c
Hayk Darbinyan <work@hayk.ar>
2024-09-07 23:02:13 -0300

    172 current  2024-09-07 23:01:59  24.05.4326.36bae4507766  6.10.6                          *

--- user/home.nix
+++ user/home.nix
@@ -118,6 +118,7 @@ in {
 
     plugins = {
       lualine.enable = true;
+      bufferline.enable = true;
       nvim-colorizer.enable = true;
       luasnip.enable = true;
       rainbow-delimiters.enable = true;
@@ -195,10 +196,11 @@ in {
           lua-ls.enable = true;
           nixd.enable = true;
           dartls.enable = true;
-          ccls = {
-            enable = true;
-            initOptions.cache.directory = "/tmp/ccls-cache";
-          };
+          clangd.enable = true;
+          #ccls = {
+          #  enable = true;
+          #  initOptions.cache.directory = "/tmp/ccls-cache";
+          #};
         };  
 
         keymaps = {
@@ -246,95 +248,7 @@ in {
       lspkind-nvim
     ];
 
-    keymaps = [
-      {
-        mode = "n";
-        key = "<F5>";
-        action.__raw = ''
-        function()
-        require('dap').continue()
-        end
-        '';
-        options = {
-          desc = "Debug: Start/Continue";
-        };
-      }
-      {
-        mode = "n";
-        key = "<F1>";
-        action.__raw = ''
-        function()
-        require('dap').step_into()
-        end
-        '';
-        options = {
-          desc = "Debug: Step Into";
-        };
-      }
-      {
-        mode = "n";
-        key = "<F2>";
-        action.__raw = ''
-        function()
-        require('dap').step_over()
-        end
-        '';
-        options = {
-          desc = "Debug: Step Over";
-        };
-      }
-      {
-        mode = "n";
-        key = "<F3>";
-        action.__raw = ''
-        function()
-        require('dap').step_out()
-        end
-        '';
-        options = {
-          desc = "Debug: Step Out";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>b";
-        action.__raw = ''
-        function()
-        require('dap').toggle_breakpoint()
-        end
-        '';
-        options = {
-          desc = "Debug: Toggle Breakpoint";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>B";
-        action.__raw = ''
-        function()
-        require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-        end
-        '';
-        options = {
-          desc = "Debug: Set Breakpoint";
-        };
-      }
-      # Toggle to see last session result. Without this, you can't see session output
-      # in case of unhandled exception.
-      {
-        mode = "n";
-        key = "<F7>";
-        action.__raw = ''
-        function()
-        require('dapui').toggle()
-        end
-        '';
-        options = {
-          desc = "Debug: See last session result.";
-        };
-      }
-
-    ];
+    keymaps = [];
     
     colorschemes.rose-pine.enable = true;
     colorschemes.rose-pine.settings = {
