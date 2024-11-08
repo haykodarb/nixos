3d26085bdccba38a49910f68ed0d87a6970ab51b
Hayk Darbinyan <work@hayk.ar>
2024-11-08 12:51:16 -0300

    230 current  2024-11-08 12:51:00  24.05.6122.080166c15633  6.11.5                          *

--- user/home.nix
+++ user/home.nix
@@ -114,13 +114,22 @@ in {
 
   programs.nixvim = {
     enable = true;
+    
+    extraConfigLua = "vim.lsp.handlers[\"textDocument/hover\"] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    border = 'rounded'
+  }
+)
 
-    diagnostics = {
-      float = {
-        border = "rounded";
-      };
-    };
-
+vim.lsp.handlers[\"textDocument/signatureHelp\"] = vim.lsp.with(
+  vim.lsp.handlers.signature_help, {
+    border = 'rounded'
+  }
+  ) vim.diagnostic.config{
+    float={border='rounded'}
+  }
+";
+  
     autoCmd = [
       {
         event = "VimEnter";
