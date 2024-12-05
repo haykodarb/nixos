3d02b85931bd51c7dad4584bb4173abd995adffb
Hayk Darbinyan <work@hayk.ar>
2024-12-05 09:49:18 -0300

    248 current  2024-12-05 09:48:39  25.05beta716003.55d15ad12a74  6.12.1                          *

--- user/home.nix
+++ user/home.nix
@@ -23,7 +23,7 @@ in {
     home.packages = with pkgs; [
       btop ripgrep fd bear jdk17 gnumake openocd qgroundcontrol thunderbird-bin slack discord
       electron lazygit catppuccin-gtk catppuccin-kde catppuccin-qt5ct catppuccin-cursors catppuccinifier-gui libreoffice-qt
-      nethogs xclip gnome.gnome-disk-utility udisks qbittorrent remmina woeusb-ng
+      nethogs xclip gnome-disk-utility udisks qbittorrent remmina woeusb-ng
       ntfs3g obs-studio kdenlive logseq marktext vlc zoxide util-linux
       calc teams-for-linux mavproxy bitwise fzf gdb cmake stremio supabase-cli
       platformio esptool freecad masterpdfeditor mtr prusa-slicer arduino nodejs_22 ugs
