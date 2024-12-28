69372cd6a69562e8451bbed4bfde24c4a33415c7
Hayk Darbinyan <work@hayk.ar>
2024-12-28 17:37:11 -0300

    267 current  2024-12-28 17:37:09  25.05beta724962.d70bd19e0a38  6.12.5                          *

--- configuration.nix
+++ configuration.nix
@@ -13,6 +13,11 @@
   nix = {
     settings.experimental-features = ["nix-command" "flakes"];
     settings.trusted-users = [ "root" "hayk" ];
+    gc = {
+	automatic = true;
+	dates = "weekly";
+	options = "--delete-older-than 30d";
+    };
   };
 
   security.sudo.wheelNeedsPassword = false;
