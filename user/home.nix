a17b91864e6912b4ae939ca9fe362a1b53a4fae5
Hayk Darbinyan <work@hayk.ar>
2024-12-31 12:32:23 -0300

    284 current  2024-12-31 12:32:21  25.05beta729683.88195a94f390  6.12.7                          *

--- user/home.nix
+++ user/home.nix
@@ -85,6 +85,20 @@ in {
         ignoreDups = true;
         ignoreAllDups = true;
       };
+
+      plugins = [
+      {
+	  name = "zsh-nix-shell";
+	  file = "nix-shell.plugin.zsh";
+	  src = pkgs.fetchFromGitHub {
+	      owner = "chisui";
+	      repo = "zsh-nix-shell";
+	      rev = "v0.8.0";
+	      sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+	  };
+      }
+      ];
+
     };
 
     programs.alacritty = {
