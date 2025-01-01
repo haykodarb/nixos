371db4f8bee110530fa5b22be06e1c882e6e8a3a
Hayk Darbinyan <work@hayk.ar>
2025-01-01 14:54:49 -0300

    289 current  2025-01-01 14:54:47  25.05beta729683.88195a94f390  6.12.7                          *

--- user/home.nix
+++ user/home.nix
@@ -77,7 +77,6 @@ in {
 	enableBashIntegration = true;
 	settings =  { 
 	    add_newline = false;
-	    format =  "$line_break $package $line_break $character";
 	    scan_timeout = 10;
 	    character = {
 		success_symbol = "➜";
