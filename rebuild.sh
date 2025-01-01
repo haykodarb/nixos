203a03f1a120df4b1834c60b903be4581d6ae882
Hayk Darbinyan <work@hayk.ar>
2025-01-01 15:24:01 -0300

    changed from zsh to bash

---
+++ rebuild.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+pushd ~/nixos
+nvim ~/nixos
+git diff -U0 
+echo "NixOS Rebuilding"
+sudo nixos-rebuild switch | (&> output.log) | tail -n 5 output.log || (cat output.log | grep --color error && false)
+gen=$(nixos-rebuild list-generations | grep current)
+git commit -am "$gen"
+git push
+popd
