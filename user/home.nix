c79fa9408925f7f95a88731442391f574131676c
Hayk Darbinyan <work@hayk.ar>
2025-01-06 15:20:07 -0300

    315 current  2025-01-06 15:20:04  25.05beta732261.6df24922a140  6.12.8                          *

--- user/home.nix
+++ user/home.nix
@@ -27,7 +27,7 @@ in {
       ntfs3g obs-studio kdenlive logseq marktext vlc zoxide util-linux
       calc teams-for-linux mavproxy bitwise fzf gdb cmake stremio supabase-cli
       platformio esptool freecad masterpdfeditor mtr prusa-slicer arduino nodejs_22 ugs
-      chromium openhantek6022 glibc nerd-fonts.hack 
+      chromium openhantek6022 glibc nerd-fonts.hack devbox 
       (python3.withPackages (python-pkgs: [
         python-pkgs.matplotlib
       ]))
@@ -172,6 +172,7 @@ in {
       lualine.enable = true;
       luasnip.enable = true;
       friendly-snippets.enable = true;
+      lsp-lines.enable = true;
 
       #mini = {
       #  enable = true;
