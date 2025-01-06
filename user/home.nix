9b64ad05fd60004171cbe0af17057dca4e269cb5
Hayk Darbinyan <work@hayk.ar>
2025-01-06 15:31:16 -0300

    316 current  2025-01-06 15:31:13  25.05beta732261.6df24922a140  6.12.8                          *

--- user/home.nix
+++ user/home.nix
@@ -27,7 +27,7 @@ in {
       ntfs3g obs-studio kdenlive logseq marktext vlc zoxide util-linux
       calc teams-for-linux mavproxy bitwise fzf gdb cmake stremio supabase-cli
       platformio esptool freecad masterpdfeditor mtr prusa-slicer arduino nodejs_22 ugs
-      chromium openhantek6022 glibc nerd-fonts.hack devbox 
+      chromium openhantek6022 glibc nerd-fonts.hack
       (python3.withPackages (python-pkgs: [
         python-pkgs.matplotlib
       ]))
