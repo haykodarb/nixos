b60de4df0680f402be845e635a0f706dff009763
Hayk Darbinyan <work@hayk.ar>
2025-03-02 11:29:42 -0300

    369 current  2025-02-28 14:50:39  25.05beta755230.73cf49b8ad83  6.13.3                          *

--- configuration.nix
+++ configuration.nix
@@ -38,7 +38,7 @@
   services.pulseaudio.extraConfig = "load-module module-combine-sink";
   hardware.graphics.enable = true;
 
-  services.pipewire.enable = false;
+  services.pipewire.enable = true;
   services.xserver.wacom.enable = true;
 
   # rtkit is optional but recommended
@@ -100,23 +100,35 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  xdg = {
+  portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-wlr
+      xdg-desktop-portal-gtk
+    ];
+    gtkUsePortal = true;
+  };
+};
+
   services.udev.packages = [ pkgs.openocd pkgs.platformio-core pkgs.platformio-core.udev ];
-  # Enable sound with pipewire.
-  #security.rtkit.enable = true;
-  #services.pipewire = {
-  #  enable = true;
-  #  alsa.enable = true;
-  #  alsa.support32Bit = true;
-  #  pulse.enable = true;
-  # If you want to use JACK applications, uncomment this
-  #  jack.enable = true;
+
 
   # use the example session manager (no others are packaged yet so this is enabled by default,
   # no need to redefine it in your config for now)
   #media-session.enable = true;
   #};
 
-  programs.firefox.enable = true;
+  programs.firefox = {
+    enable = true;
+    package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
+      forceWayland = true;
+      extraPolicies = {
+        ExtensionSettings = {};
+      };
+    };
+  };
+
   programs.nix-ld.enable = true;
 
   programs.steam = {
